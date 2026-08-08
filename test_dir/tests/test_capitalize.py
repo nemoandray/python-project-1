c25859cee97eda16a695124e800cc42b55e96f3c
AndrewNemo <andrej.isaev1989@gmail.com>
2026-08-08 13:48:32 +0300

    Changed test_capitalize.py

--- test_dir/tests/test_capitalize.py
+++ test_dir/tests/test_capitalize.py
@@ -1,9 +1,7 @@
-from src.capitalize import capitalize
+from capitalize import capitalize
 
-if capitalize("hello") != "Hello":
-    raise Exception("Функция работает неверно!")
+assert capitalize("hello") == "Hello"
 
-if capitalize("") != "":
-    raise Exception("Функция работает неверно!")
+assert capitalize("") == ""
 
 print("Все тесты пройдены!")
